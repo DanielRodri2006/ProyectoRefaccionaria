d9bec32c97621fba60e3367567a764ca0ddbca73
rodri <rodri@192.168.56.1>
2025-04-11 15:49:32 -0700

    cree el proyecto

---
+++ PROYECTOrefa/src/main/java/com/mycompany/proyectorefa/PROYECTOrefa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectorefa;
+
+/**
+ *
+ * @author rodri
+ */
+public class PROYECTOrefa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
